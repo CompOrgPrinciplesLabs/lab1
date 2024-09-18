38deb3a005d8bead955d98332aa4de2e5018d001
Vlad <76902422+gnat-vasylevych@users.noreply.github.com>
2024-09-18 20:45:04 +0300

    Vlad (#1)
    * implement at()
    
    * implement reserve()
    
    * implement shrink_to_fit()
    
    * implement clear()
    
    * initial logic of insert()
    
    * fix reserve()
    
    * finish insert()
    
    * implement insert() for single char
    
    * implement insert() for C string
    
    * implement all append() functions
    
    * implement erase()
    
    * implement erase()
    
    * implement size() and capacity()
    
    * implement c_str()
    
    * implement all find() functions

--- mystring.cpp
+++ mystring.cpp
@@ -1,11 +1,14 @@
-//
-// // Created by Вероника Багатырь-Захарченко on 12.09.2024.
-// //
+// This is a personal academic project. Dear PVS-Studio, please check it.
+
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: https://pvs-studio.com
+
 #include "mystring.h"
 #include <string>
 #include <cstring>
 #include <stdexcept>
 #include <algorithm>
+#include <iostream>
+#include <ostream>
 
 size_t calc_capacity(size_t size) {
     return size*2+1 ;
@@ -14,16 +17,20 @@ size_t calc_capacity(size_t size) {
 int compare_two_strings(const char* str1, const char* str2) {
     size_t l1 = std::strlen(str1);
     size_t l2 = std::strlen(str2);
-    for (size_t i = 0; i < std::min(l1, l2); ++i) {
+    const size_t min_len = std::min(l1,l2);
+
+    for (size_t i = 0; i < min_len; ++i) {
         if (str1[i] < str2[i]) {
             return -1;  // str1 < str2
-        } else if (str1[i] > str2[i]) {
+        }
+        if (str1[i] > str2[i]) {
             return 1;   // str1 > str2
         }
     }
     if (l1 < l2) {
         return -1;
-    } else if (l1 > l2) {
+    }
+    if (l1 > l2) {
         return 1;
     }
     return 0; //рівні
@@ -81,6 +88,7 @@ my_str_t::~my_str_t() {
     delete[] data_m;
 }
 
+// author Vlad Vasylevych
 void my_str_t::swap(my_str_t& mystr) noexcept{
     const size_t tmp_size = size_m;
     const size_t tmp_cap = capacity_m;
@@ -94,6 +102,238 @@ void my_str_t::swap(my_str_t& mystr) noexcept{
     mystr.capacity_m = tmp_cap;
     mystr.data_m = tmp_data_m;
 }
+
+// author Vlad Vasylevych
+char &my_str_t::at(size_t idx) {
+    if (idx >= size_m) {
+        throw std::out_of_range("my_str_t::at");
+    }
+    return data_m[idx];
+}
+
+// author Vlad Vasylevych
+const char &my_str_t::at(size_t idx) const{
+    if (idx >= size_m) {
+        throw std::out_of_range("my_str_t::at");
+    }
+    return data_m[idx];
+}
+
+// author Vlad Vasylevych
+void my_str_t::reserve(size_t new_capacity) {
+    if (new_capacity < capacity_m) {
+        return;
+    }
+    capacity_m = new_capacity;
+
+    char* new_data_m = new char[capacity_m];
+    std::memcpy(new_data_m, data_m, size_m);
+    delete[] data_m;
+    data_m = new_data_m;
+}
+
+// author Vlad Vasylevych
+void my_str_t::shrink_to_fit() {
+    char* new_data_m = new char[size_m];
+    std::memcpy(new_data_m, data_m, size_m);
+    delete[] data_m;
+    data_m = new_data_m;
+}
+
+// author Vlad Vasylevych
+void my_str_t::clear() {
+    size_m = 0;
+    delete[] data_m; // not sure I should delete
+}
+
+// author Vlad Vasylevych
+void my_str_t::insert(size_t idx, const my_str_t &str) {
+    if (idx > size_m) {
+        throw std::out_of_range("my_str_t::insert");
+    }
+
+    if (size_m + str.size_m > capacity_m) {
+        const size_t new_cap = (size_m + str.size_m) * 2;
+        reserve(new_cap);
+    }
+
+    // insert logic
+    std::memmove(data_m + idx + str.size_m, data_m + idx, size_m - idx);
+    std::memmove(data_m + idx, str.data_m, str.size_m);
+
+    size_m = size_m + str.size_m;
+
+}
+
+// author Vlad Vasylevych
+void my_str_t::insert(size_t idx, char c) {
+    if (idx > size_m) {
+        throw std::out_of_range("my_str_t::insert");
+    }
+
+    if (size_m + 1 > capacity_m) {
+        const size_t new_cap = (size_m + 1) * 2;
+        reserve(new_cap);
+    }
+
+    // insert logic
+    std::memmove(data_m + idx + 1, data_m + idx, size_m - idx);
+    std::memmove(data_m + idx, &c, 1);
+
+    size_m = size_m + 1;
+}
+
+// author Vlad Vasylevych
+void my_str_t::insert(size_t idx, const char *cstr) {
+    if (idx > size_m) {
+        throw std::out_of_range("my_str_t::insert");
+    }
+
+    const size_t cstr_size = std::strlen(cstr);
+
+    if (size_m + cstr_size > capacity_m) {
+        const size_t new_cap = (size_m + cstr_size) * 2;
+        reserve(new_cap);
+    }
+
+    // insert logic
+    std::memmove(data_m + idx + cstr_size, data_m + idx, size_m - idx);
+    std::memmove(data_m + idx, cstr, cstr_size);
+
+    size_m = size_m + cstr_size;
+}
+
+// author Vlad Vasylevych
+void my_str_t::append(const my_str_t &str) {
+    if (size_m + str.size_m > capacity_m) {
+        const size_t new_cap = (size_m + str.size_m) * 2;
+        reserve(new_cap);
+    }
+
+    std::memcpy(data_m + size_m, str.data_m, str.size_m);
+
+    size_m = size_m + str.size_m;
+}
+
+// author Vlad Vasylevych
+void my_str_t::append(char c) {
+    if (size_m + 1 > capacity_m) {
+        const size_t new_cap = (size_m + 1) * 2;
+        reserve(new_cap);
+    }
+
+    std::memcpy(data_m + size_m, &c, 1);
+
+    size_m = size_m + 1;
+}
+
+// author Vlad Vasylevych
+void my_str_t::append(const char* cstr) {
+    const size_t cstr_size = std::strlen(cstr);
+
+    if (size_m + cstr_size > capacity_m) {
+        const size_t new_cap = (size_m + cstr_size) * 2;
+        reserve(new_cap);
+    }
+
+    std::memcpy(data_m + size_m, cstr, cstr_size);
+
+    size_m = size_m + cstr_size;
+}
+
+// author Vlad Vasylevych
+void my_str_t::erase(size_t begin, size_t size) {
+    if (begin > size_m) {
+        throw std::out_of_range("my_str_t::erase");
+    }
+
+    const size_t right_size = std::min(size, size_m - begin);
+
+    std::memcpy(data_m + begin, data_m + begin + right_size, size_m - begin - right_size);
+    size_m = size_m - right_size;
+}
+
+// author Vlad Vasylevych
+size_t my_str_t::size() const noexcept{
+    return size_m;
+}
+
+// author Vlad Vasylevych
+size_t my_str_t::capacity() const noexcept{
+    return capacity_m;
+}
+
+// author Vlad Vasylevych
+const char* my_str_t::c_str() const {
+    return data_m;
+}
+
+// author Vlad Vasylevych
+size_t my_str_t::find(char c, size_t idx) {
+    if (idx > size_m) {
+        throw std::out_of_range("my_str_t::find");
+    }
+
+    for (size_t i = idx; i < size_m; i++) {
+        if (data_m[i] == c) {
+            return i;
+        }
+    }
+
+    return not_found;
+
+}
+
+// author Vlad Vasylevych
+size_t my_str_t::find(const std::string& s, size_t idx) {
+    if (idx > size_m) {
+        throw std::out_of_range("my_str_t::find");
+    }
+
+    const size_t s_len = s.length();
+
+    for (size_t i = idx; i < size_m - s_len; i++) {
+        bool is_same = true;
+        for (size_t j = 0; j < s_len; j++) {
+            if (data_m[i + j] == s[j]) {
+                continue;
+            }
+            is_same = false;
+            break;
+        }
+        if (is_same == true) {
+            return i;
+        }
+    }
+
+    return not_found;
+}
+
+// author Vlad Vasylevych
+size_t my_str_t::find(const char* cstr, size_t idx) {
+    if (idx > size_m) {
+        throw std::out_of_range("my_str_t::find");
+    }
+
+    const size_t s_len = std::strlen(cstr);
+
+    for (size_t i = idx; i < size_m - s_len; i++) {
+        bool is_same = true;
+        for (size_t j = 0; j < s_len; j++) {
+            if (data_m[i + j] == cstr[j]) {
+                continue;
+            }
+            is_same = false;
+            break;
+        }
+        if (is_same == true) {
+            return i;
+        }
+    }
+
+    return not_found;
+}
+
 
 char& my_str_t::operator[](size_t idx) {
     return data_m[idx];
--- mystring.h
+++ mystring.h
@@ -6,14 +6,14 @@
 
 class my_str_t {
 private:
-    // char* data_m;         // Вказівник на блок пам'яті
-    // size_t capacity_m;    // Розмір блока
-    // size_t size_m;        // Фактичний розмір стрічки
+    char* data_m;         // Вказівник на блок пам'яті
+    size_t capacity_m;    // Розмір блока
+    size_t size_m;        // Фактичний розмір стрічки
 
 public:
-    size_t capacity_m;    // Розмір блока
-    size_t size_m;
-    char* data_m;
+    // size_t capacity_m;    // Розмір блока
+    // size_t size_m;
+    // char* data_m;
     // Конструктор: створює стрічку із 'size' копій літери 'initial'
     my_str_t(size_t size, char initial);
 
@@ -52,6 +52,67 @@ public:
 
     // Очищення стрічки
     void clear();
+
+    //! Вставляє передану стрічку типу my_str_t, чи літеру,
+    //! чи С-стрічку, починаючи з літери idx,
+    //! зсуваючи літеру з позиції idx і правіше праворуч.
+    //! Обробка помилок:
+    //! Якщо idx > size_m -- кидає виключення std::out_of_range
+    void insert(size_t idx, const my_str_t& str);
+
+    //! Ця функція -- служить для оптимізації, щоб не
+    //! довелося спочатку створювати із літери c стрічку my_str_t, а
+    //! потім вставляти. Навіть якщо компілятор зробив би це
+    //! автоматично -- це повільно.
+    void insert(size_t idx, char c);
+
+    //! Аналогічна оптимізація для C-стрічок
+    void insert(size_t idx, const char* cstr);
+
+    //! Додає стрічку чи літеру в кінці стрічки.
+    //! Обробка помилок:
+        //! Не повинні заважати пропагуванню виключення
+        //! std::bad_alloc.
+    void append(const my_str_t& str);
+
+    void append(char c);
+
+    void append(const char* cstr);
+
+    //! Видаляє size літер, починаючи з індексу begin.
+    //! Літери праворуч від видаленого шматка зсуваються
+    //! на size ліворуч.
+    //! Обробка помилок:
+    //! Якщо begin > size_m -- кидає виключення std::out_of_range.
+    //! Але якщо стрічка містить менше символів для стирання,
+    //! ніж потрібно -- це не помилка, видаляє всі до кінця.
+    void erase(size_t begin, size_t size);
+
+    //! Наступні функції повертають розмір стрічки та
+    //! зарезервований блок пам'яті.
+    //! Правильніше б назвати get_size() та get_capacity(),
+    //! але історично в std::string прийнято
+    //! так, то будемо наслідувати його
+    size_t size() const noexcept;
+    size_t capacity() const noexcept;
+
+    //! Повертає вказівник на С-стрічку, яка відповідає
+    //! цьому об'єкту. Якщо виділяти один зайвий байт,
+    //! як описано в документації на конструтор, то їй
+    //! достатньо зберегти нульовий байт у позиції
+    //! data_m[size_m] і повернути data_m.
+    const char* c_str() const;
+
+    //! Функції find шукають вказану літеру чи стрічку,
+    //! починаючи із idx. Якщо не знайдено, повертає
+    //! константу my_str_t::not_found
+    //! Обробка помилок:
+    //! Якщо idx > size_m -- кидає виключення std::out_of_range.
+    static constexpr size_t not_found = -1;
+    size_t find(char c, size_t idx = 0);
+    size_t find(const std::string& str, size_t idx = 0);
+    size_t find(const char* cstr, size_t idx = 0);
+
     friend bool operator==(const my_str_t& str1, const my_str_t& str2);
     friend bool operator!=(const my_str_t& str1, const my_str_t& str2);
     friend bool operator> (const my_str_t& str1, const my_str_t& str2);
