87c30842c2a267fba52314894586dc5089953b53
Veronika Bahatyr-Zakharchenko <bahatyr.pn@ucu.edu.ua>
2024-09-19 20:51:59 +0300

    readded all

---
+++ testing/mystring.cpp
@@ -0,0 +1,684 @@
+// This is a personal academic project. Dear PVS-Studio, please check it.
+
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: https://pvs-studio.com
+
+#include "mystring.hpp"
+#include <string>
+#include <cstring>
+#include <stdexcept>
+#include <algorithm>
+#include <iostream>
+#include <ostream>
+
+// author Veronika Bahatyr-Zakharchenko
+size_t calc_capacity(size_t size) {
+    return size*2+1 ;
+}
+
+// author Veronika Bahatyr-Zakharchenko
+int compare_two_strings(const char* str1, const char* str2, const size_t str1_size, const size_t str2_size) {
+    // size_t l1 = std::strlen(str1);
+    // size_t l2 = std::strlen(str2);
+    // const size_t min_len = std::min(str1_size, str2_size)-1;
+    size_t i = 0;
+    while (str1[i] != '\0' && str2[i] != '\0') {
+        if (str1[i] < str2[i]) {
+            return -1;  // str1 < str2
+        }
+        if (str1[i] > str2[i]) {
+            return 1;   // str1 > str2
+        }
+        i++;
+    }
+
+    if (str1_size < str2_size) {
+        return -1;
+    }
+    if (str1_size > str2_size) {
+        return 1;
+    }
+    return 0; //рівні
+}
+
+// author Kassiia Tserkovna
+my_str_t::my_str_t(size_t size, char initial) {
+    capacity_m = calc_capacity(size);
+    size_m = size;
+    data_m = new char[capacity_m];
+    for (size_t i = 0; i < size; i++) {
+        data_m[i] = initial;
+    }
+    data_m[size] = '\0';
+}
+
+// author Veronika Bahatyr-Zakharchenko
+my_str_t::my_str_t(const char* cstr) {
+    size_m = strlen(cstr);
+    capacity_m = calc_capacity(size_m);
+    data_m = new char[capacity_m];
+    for (size_t i = 0; i < size_m; i++) {
+        data_m[i] = cstr[i];
+    }
+    data_m[size_m] = '\0';
+}
+
+// author Veronika Bahatyr-Zakharchenko
+my_str_t::my_str_t(const std::string& str) {
+    size_m = str.size();
+    capacity_m = calc_capacity(size_m);
+    data_m = new char[capacity_m];
+    for (size_t i = 0; i < size_m; i++) {
+        data_m[i] = str[i];
+    }
+    // std::memcpy(data_m, &str, size_m); // why not?
+    data_m[size_m] = '\0';
+}
+
+// author Veronika Bahatyr-Zakharchenko
+my_str_t::my_str_t(const my_str_t& mystr) {
+    size_m = mystr.size_m;
+    capacity_m = mystr.capacity_m;
+    data_m = new char[capacity_m];
+    std::memcpy(data_m, mystr.data_m, size_m + 1); // новий блок памʼяті; блок, з якого копіюємо; кількість бітів + терм
+}
+
+// author Veronika Bahatyr-Zakharchenko
+my_str_t& my_str_t::operator=(const my_str_t& mystr) {
+    if (this != &mystr) {
+        delete[] data_m;
+        size_m = mystr.size_m;
+        capacity_m = mystr.capacity_m;
+        data_m = new char[capacity_m];
+        std::memcpy(data_m, mystr.data_m, size_m + 1);
+    }
+    return *this;
+}
+
+// author Veronika Bahatyr-Zakharchenko
+my_str_t& my_str_t::operator+=(const my_str_t& str) {
+    append(str.data_m);
+    return *this;
+}
+
+// author Veronika Bahatyr-Zakharchenko
+my_str_t& my_str_t::operator+=(const char* cstr1) {
+    append((cstr1));
+    return *this;
+}
+
+// author Veronika Bahatyr-Zakharchenko
+my_str_t& my_str_t::operator+=(char c) {
+    append(c);
+    return *this;
+}
+
+// author Veronika Bahatyr-Zakharchenko
+my_str_t operator+(const my_str_t& str1, const my_str_t& str2) {
+    my_str_t result(str1);
+    result += str2;
+    return result;
+}
+
+// author Veronika Bahatyr-Zakharchenko
+my_str_t operator+(const my_str_t& str1, const char* cstr2) {
+    my_str_t result(str1);
+    result += cstr2;
+    return result;
+}
+
+// author Veronika Bahatyr-Zakharchenko
+my_str_t operator+(const my_str_t& str1, char c) {
+    my_str_t result(str1);
+    result += c;
+    return result;
+}
+
+// author Veronika Bahatyr-Zakharchenko
+my_str_t& my_str_t::operator*=(const size_t num) {
+    size_m = size_m * num;
+    capacity_m = calc_capacity(size_m);
+    for (size_t i = 1; i < num; ++i) {
+        std::memcpy(data_m + (i * size_m), data_m, size_m);
+    }
+    data_m[size_m] = '\0';
+    return *this;
+}
+
+// author Veronika Bahatyr-Zakharchenko
+my_str_t operator*(const my_str_t& str, size_t num) {
+    my_str_t result(str);
+    result *= num;
+    return result;
+}
+
+// author Veronika Bahatyr-Zakharchenko
+my_str_t::~my_str_t() {
+    delete[] data_m;
+}
+
+// author Vlad Vasylevych
+void my_str_t::swap(my_str_t& mystr) noexcept{
+    const size_t tmp_size = size_m;
+    const size_t tmp_cap = capacity_m;
+    char* tmp_data_m = data_m;
+
+    size_m = mystr.size_m;
+    capacity_m = mystr.capacity_m;
+    data_m = mystr.data_m;
+
+    mystr.size_m = tmp_size;
+    mystr.capacity_m = tmp_cap;
+    mystr.data_m = tmp_data_m;
+}
+
+// author Vlad Vasylevych
+char &my_str_t::at(size_t idx) {
+    if (idx >= size_m) {
+        throw std::out_of_range("my_str_t::at");
+    }
+    return data_m[idx];
+}
+
+// author Vlad Vasylevych
+const char &my_str_t::at(size_t idx) const{
+    if (idx >= size_m) {
+        throw std::out_of_range("my_str_t::at");
+    }
+    return data_m[idx];
+}
+
+// author Vlad Vasylevych
+void my_str_t::reserve(size_t new_capacity) {
+    if (new_capacity < capacity_m) {
+        return;
+    }
+    capacity_m = new_capacity;
+
+    char* new_data_m = new char[capacity_m];
+    std::memcpy(new_data_m, data_m, size_m);
+    delete[] data_m;
+    data_m = new_data_m;
+}
+
+// author Veronika Bahatyr-Zakharchenko
+void my_str_t::resize(size_t new_size, char new_char) {
+    if (new_size < size_m) {
+        size_m = new_size;
+    } else if (new_size <= capacity_m) {
+        for (size_t i = size_m; i < new_size; ++i) {
+            data_m[i] = new_char;
+        }
+        size_m = new_size;
+    } else {
+        reserve(new_size);
+        for (size_t i = size_m; i < new_size; ++i) {
+            data_m[i] = new_char;
+        }
+        size_m = new_size;
+    }
+    data_m[size_m] = '\0';
+}
+
+// author Veronika Bahatyr-Zakharchenko
+my_str_t my_str_t::substr(size_t begin, size_t size) {
+    if (begin > size_m) {
+        throw std::out_of_range("index is out of range");
+    }
+    if (begin + size > size_m) {
+        size = size_m - begin;
+    }
+    my_str_t result(size, ' ');
+    for (size_t i = 0; i < size; ++i) {
+        result[i] = data_m[begin + i];
+    }
+    result[size] = '\0';
+    return result;
+}
+
+
+
+// author Vlad Vasylevych
+void my_str_t::shrink_to_fit() {
+    char* new_data_m = new char[size_m+1];
+    std::memcpy(new_data_m, data_m, size_m);
+    delete[] data_m;
+    data_m = new_data_m;
+    capacity_m = size_m+1;
+    data_m[size_m] = '\0';
+}
+
+// author Kassiia Tserkovna
+void my_str_t::clear() {
+    size_m = 0;
+    capacity_m = 1;
+    delete[] data_m;
+    data_m = new char[capacity_m];
+    data_m[0] = '\0';
+}
+
+// author Vlad Vasylevych
+void my_str_t::insert(size_t idx, const my_str_t &str) {
+    if (idx > size_m) {
+        throw std::out_of_range("my_str_t::insert");
+    }
+
+    if (size_m + str.size_m > capacity_m) {
+        const size_t new_cap = (size_m + str.size_m) * 2;
+        reserve(new_cap);
+    }
+
+    // insert logic
+    std::memmove(data_m + idx + str.size_m, data_m + idx, size_m - idx);
+    std::memmove(data_m + idx, str.data_m, str.size_m);
+
+    size_m = size_m + str.size_m;
+
+}
+
+// author Vlad Vasylevych
+void my_str_t::insert(size_t idx, char c) {
+    if (idx > size_m) {
+        throw std::out_of_range("my_str_t::insert");
+    }
+
+    if (size_m + 1 > capacity_m) {
+        const size_t new_cap = (size_m + 1) * 2;
+        reserve(new_cap);
+    }
+
+    // insert logic
+    std::memmove(data_m + idx + 1, data_m + idx, size_m - idx);
+    std::memmove(data_m + idx, &c, 1);
+
+    size_m = size_m + 1;
+}
+
+// author Vlad Vasylevych
+void my_str_t::insert(size_t idx, const char *cstr) {
+    if (idx > size_m) {
+        throw std::out_of_range("my_str_t::insert");
+    }
+
+    const size_t cstr_size = std::strlen(cstr);
+
+    if (size_m + cstr_size > capacity_m) {
+        const size_t new_cap = (size_m + cstr_size) * 2;
+        reserve(new_cap);
+    }
+
+    // insert logic
+    std::memmove(data_m + idx + cstr_size, data_m + idx, size_m - idx);
+    std::memmove(data_m + idx, cstr, cstr_size);
+
+    size_m = size_m + cstr_size;
+}
+
+// author Vlad Vasylevych
+void my_str_t::append(const my_str_t &str) {
+    if (size_m + str.size_m > capacity_m) {
+        const size_t new_cap = (size_m + str.size_m) * 2;
+        reserve(new_cap);
+    }
+
+    std::memcpy(data_m + size_m, str.data_m, str.size_m);
+
+    size_m = size_m + str.size_m;
+    data_m[size_m] = '\0';
+
+}
+
+// author Vlad Vasylevych
+void my_str_t::append(char c) {
+    if (size_m + 1 > capacity_m) {
+        const size_t new_cap = (size_m + 1) * 2;
+        reserve(new_cap);
+    }
+
+    std::memcpy(data_m + size_m, &c, 1);
+
+    size_m = size_m + 1;
+    data_m[size_m] = '\0';
+
+}
+
+// author Vlad Vasylevych
+void my_str_t::append(const char* cstr) {
+    const size_t cstr_size = std::strlen(cstr);
+
+    if (size_m + cstr_size > capacity_m) {
+        const size_t new_cap = (size_m + cstr_size) * 2;
+        reserve(new_cap);
+    }
+
+    std::memcpy(data_m + size_m, cstr, cstr_size);
+
+
+    size_m = size_m + cstr_size;
+    data_m[size_m] = '\0';
+}
+
+// author Vlad Vasylevych
+void my_str_t::erase(size_t begin, size_t size) {
+    if (begin > size_m) {
+        throw std::out_of_range("my_str_t::erase");
+    }
+
+    const size_t right_size = std::min(size, size_m - begin);
+
+    std::memcpy(data_m + begin, data_m + begin + right_size, size_m - begin - right_size);
+    size_m = size_m - right_size;
+    data_m[size_m] = '\0';
+}
+
+// author Vlad Vasylevych
+size_t my_str_t::size() const noexcept{
+    return size_m;
+}
+
+// author Vlad Vasylevych
+size_t my_str_t::capacity() const noexcept{
+    return capacity_m;
+}
+
+// author Vlad Vasylevych
+const char* my_str_t::c_str() const {
+    return data_m;
+}
+
+// author Vlad Vasylevych
+size_t my_str_t::find(char c, size_t idx) {
+    if (idx > size_m) {
+        throw std::out_of_range("my_str_t::find");
+    }
+
+    for (size_t i = idx; i < size_m; i++) {
+        if (data_m[i] == c) {
+            return i;
+        }
+    }
+
+    return not_found;
+
+}
+
+// author Vlad Vasylevych
+size_t my_str_t::find(const std::string& s, size_t idx) {
+    if (idx > size_m) {
+        throw std::out_of_range("my_str_t::find");
+    }
+
+    const size_t s_len = s.length();
+
+    if (size_m < s_len) {
+        return not_found;
+    }
+
+    if (size_m ==0 && s_len == 0) {
+        return not_found;
+    }
+
+    for (size_t i = idx; i <= size_m - s_len; i++) {
+        bool is_same = true;
+        for (size_t j = 0; j < s_len; j++) {
+            if (data_m[i + j] == s[j]) {
+                continue;
+            }
+            is_same = false;
+            break;
+        }
+        if (is_same == true) {
+            return i;
+        }
+    }
+
+    return not_found;
+}
+
+// author Vlad Vasylevych
+size_t my_str_t::find(const char* cstr, size_t idx) {
+    if (idx > size_m) {
+        throw std::out_of_range("my_str_t::find");
+    }
+
+    const size_t s_len = std::strlen(cstr);
+
+    if (size_m < s_len) {
+        return not_found;
+    }
+
+    if (size_m ==0 && s_len == 0) {
+        return not_found;
+    }
+
+
+    for (size_t i = idx; i <= size_m - s_len; i++) {
+        bool is_same = true;
+        for (size_t j = 0; j < s_len; j++) {
+            if (data_m[i + j] == cstr[j]) {
+                continue;
+            }
+            is_same = false;
+            break;
+        }
+        if (is_same == true) {
+            return i;
+        }
+    }
+
+    return not_found;
+}
+
+// author Veronika Bahatyr-Zakharchenko
+char& my_str_t::operator[](size_t idx) {
+    return data_m[idx];
+}
+
+// author Veronika Bahatyr-Zakharchenko
+const char& my_str_t::operator[](size_t idx) const {
+    return data_m[idx];
+}
+
+// author Kassiia Tserkovna
+std::ostream& operator<<(std::ostream& stream, const my_str_t& str) {
+    for (size_t i = 0; i < str.size_m; i++) {
+        stream << str[i];
+    }
+    return stream;
+}
+
+// author Kassiia Tserkovna
+std::istream& operator>>(std::istream& stream, my_str_t& str){
+    str.clear();
+    char temp;
+    do {
+        stream.get(temp);
+        if (stream.peek()==EOF) {
+            return stream;
+        }
+    } while (isspace(temp));
+    str.append(temp);
+    do {
+        stream.get(temp);
+        str.append(temp);
+    } while (!isspace(temp) && stream.peek() != EOF);
+    return stream;
+}
+
+
+
+
+// author Veronika Bahatyr-Zakharchenko
+bool operator==(const my_str_t& str1, const my_str_t& str2) {
+    int rez = compare_two_strings(str1.data_m, str2.data_m, str1.size_m, str2.size_m);
+    if (rez == 0) {
+        return true;
+    }
+    return false;
+}
+
+// author Veronika Bahatyr-Zakharchenko
+bool operator!=(const my_str_t& str1, const my_str_t& str2) {
+    int rez = compare_two_strings(str1.data_m, str2.data_m, str1.size_m, str2.size_m);
+    if (rez == 0) {
+        return false;
+    }
+    return true;
+}
+
+
+// author Veronika Bahatyr-Zakharchenko
+bool operator> (const my_str_t& str1, const my_str_t& str2) {
+    int rez = compare_two_strings(str1.data_m, str2.data_m, str1.size_m, str2.size_m);
+    if (rez == 1) {
+        return true;
+    }
+    return false;
+}
+
+// author Veronika Bahatyr-Zakharchenko
+bool operator>=(const my_str_t& str1, const my_str_t& str2) {
+    int rez = compare_two_strings(str1.data_m, str2.data_m, str1.size_m, str2.size_m);
+    if (rez == 0 || rez == 1) {
+        return true;
+    }
+    return false;
+}
+
+// author Veronika Bahatyr-Zakharchenko
+bool operator< (const my_str_t& str1, const my_str_t& str2) {
+    int rez = compare_two_strings(str1.data_m, str2.data_m, str1.size_m, str2.size_m);
+    if (rez == -1) {
+        return true;
+    }
+    return false;
+}
+
+// author Veronika Bahatyr-Zakharchenko
+bool operator<=(const my_str_t& str1, const my_str_t& str2) {
+    int rez = compare_two_strings(str1.data_m, str2.data_m, str1.size_m, str2.size_m);
+    if (rez == 0 || rez == -1) {
+        return true;
+    }
+    return false;
+}
+
+// //! Same for the const char*
+
+// author Veronika Bahatyr-Zakharchenko
+bool operator==(const my_str_t& str1, const char* cstr2) {
+    int rez = compare_two_strings(str1.data_m, cstr2, str1.size_m, std::strlen(cstr2));
+    if (rez == 0) {
+        return true;
+    }
+    return false;
+}
+
+// author Veronika Bahatyr-Zakharchenko
+bool operator!=(const my_str_t& str1, const char* cstr2) {
+    int rez = compare_two_strings(str1.data_m, cstr2, str1.size_m, std::strlen(cstr2));
+    if (rez == 0) {
+        return false;
+    }
+    return true;
+}
+
+// author Veronika Bahatyr-Zakharchenko
+bool operator> (const my_str_t& str1, const char* cstr2) {
+    int rez = compare_two_strings(str1.data_m, cstr2, str1.size_m, std::strlen(cstr2));
+    if (rez == 1) {
+        return true;
+    }
+    return false;
+}
+
+// author Veronika Bahatyr-Zakharchenko
+bool operator>=(const my_str_t& str1, const char* cstr2) {
+    int rez = compare_two_strings(str1.data_m, cstr2, str1.size_m, std::strlen(cstr2));
+    if (rez == 0||rez == 1) {
+        return true;
+    }
+    return false;
+}
+
+// author Veronika Bahatyr-Zakharchenko
+bool operator< (const my_str_t& str1, const char* cstr2) {
+    int rez = compare_two_strings(str1.data_m, cstr2, str1.size_m, std::strlen(cstr2));
+    if (rez == -1) {
+        return true;
+    }
+    return false;
+}
+
+// author Veronika Bahatyr-Zakharchenko
+bool operator<=(const my_str_t& str1, const char* cstr2) {
+    int rez = compare_two_strings(str1.data_m, cstr2, str1.size_m, std::strlen(cstr2));
+    if (rez == 0||rez == -1) {
+        return true;
+    }
+    return false;
+}
+
+// author Veronika Bahatyr-Zakharchenko
+bool operator==(const char* cstr1, const my_str_t& str2) {
+    int rez = compare_two_strings(cstr1, str2.data_m, std::strlen(cstr1), str2.size_m);
+    if (rez == 0) {
+        return true;
+    }
+    return false;
+}
+
+// author Veronika Bahatyr-Zakharchenko
+bool operator!=(const char* cstr1, const my_str_t& str2) {
+    int rez = compare_two_strings(cstr1, str2.data_m, std::strlen(cstr1), str2.size_m);
+    if (rez == 0) {
+        return false;
+    }
+    return true;
+}
+
+// author Veronika Bahatyr-Zakharchenko
+bool operator> (const char* cstr1, const my_str_t& str2) {
+    int rez = compare_two_strings(cstr1, str2.data_m, std::strlen(cstr1), str2.size_m);
+    if (rez == 1) {
+        return true;
+    }
+    return false;
+}
+
+// author Veronika Bahatyr-Zakharchenko
+bool operator>=(const char* cstr1, const my_str_t& str2) {
+    int rez = compare_two_strings(cstr1, str2.data_m, std::strlen(cstr1), str2.size_m);
+    if (rez == 0||rez == 1) {
+        return true;
+    }
+    return false;
+}
+
+// author Veronika Bahatyr-Zakharchenko
+bool operator< (const char* cstr1, const my_str_t& str2) {
+    int rez = compare_two_strings(cstr1, str2.data_m, std::strlen(cstr1), str2.size_m);
+    if (rez == -1) {
+        return true;
+    }
+    return false;
+}
+
+// author Veronika Bahatyr-Zakharchenko
+bool operator<=(const char* cstr1, const my_str_t& str2) {
+    int rez = compare_two_strings(cstr1, str2.data_m, std::strlen(cstr1), str2.size_m);
+    if (rez == 0||rez == -1) {
+        return true;
+    }
+    return false;
+}
+
+// author Kassiia Tserkovna
+std::istream& readline(std::istream& stream, my_str_t& str) {
+    str.clear();
+    char temp;
+    while (stream.peek() != '\n') {
+        stream.get(temp);
+        str.append(temp);
+    }
+    return stream;
+}
---
+++ testing/mystring.hpp
@@ -0,0 +1,163 @@
+#ifndef MYSTRING_H
+#define MYSTRING_H
+
+#include <cstddef>  // для size_t
+#include <stdexcept>  // для std::out_of_range
+
+class my_str_t {
+private:
+    // char* data_m;         // Вказівник на блок пам'яті
+    // size_t capacity_m;    // Розмір блока
+    // size_t size_m;        // Фактичний розмір стрічки
+
+public:
+    size_t capacity_m;    // Розмір блока
+    size_t size_m;
+    char* data_m;
+    // Конструктор: створює стрічку із 'size' копій літери 'initial'
+    my_str_t(size_t size, char initial);
+
+    // Конструктор: копіює вміст C-стрічки
+    my_str_t(const char* cstr);
+    my_str_t(const std::string& str);
+    // Конструктор копіювання
+    my_str_t(const my_str_t& mystr);
+
+    // Оператор присвоєння
+    my_str_t& operator=(const my_str_t& mystr);
+
+    my_str_t& operator+=(const my_str_t& mystr);
+    my_str_t& operator+=(const char* cstr1);
+    my_str_t& operator+=(char c);
+    my_str_t& operator*=(const size_t num);
+
+    void swap(my_str_t& mystr) noexcept;
+
+    // Деструктор
+    ~my_str_t();
+
+    // Оператор доступу за індексом (з можливістю модифікації)
+    char& operator[](size_t idx);
+
+    // Оператор доступу за індексом (без можливості модифікації)
+    const char& operator[](size_t idx) const;
+
+    // Метод отримання символа за індексом з перевіркою меж
+    char& at(size_t idx);
+    const char& at(size_t idx) const;
+
+    // Збільшення резерву пам'яті
+    void reserve(size_t new_capacity);
+
+    // Зменшення резерву пам'яті
+    void shrink_to_fit();
+
+    // Зміна розміру стрічки
+    void resize(size_t new_size, char new_char = ' ');
+
+    // Очищення стрічки
+    void clear();
+
+    //! Вставляє передану стрічку типу my_str_t, чи літеру,
+    //! чи С-стрічку, починаючи з літери idx,
+    //! зсуваючи літеру з позиції idx і правіше праворуч.
+    //! Обробка помилок:
+    //! Якщо idx > size_m -- кидає виключення std::out_of_range
+    void insert(size_t idx, const my_str_t& str);
+
+    //! Ця функція -- служить для оптимізації, щоб не
+    //! довелося спочатку створювати із літери c стрічку my_str_t, а
+    //! потім вставляти. Навіть якщо компілятор зробив би це
+    //! автоматично -- це повільно.
+    void insert(size_t idx, char c);
+
+    //! Аналогічна оптимізація для C-стрічок
+    void insert(size_t idx, const char* cstr);
+
+    //! Додає стрічку чи літеру в кінці стрічки.
+    //! Обробка помилок:
+        //! Не повинні заважати пропагуванню виключення
+        //! std::bad_alloc.
+    void append(const my_str_t& str);
+
+    void append(char c);
+
+    void append(const char* cstr);
+
+    //! Видаляє size літер, починаючи з індексу begin.
+    //! Літери праворуч від видаленого шматка зсуваються
+    //! на size ліворуч.
+    //! Обробка помилок:
+    //! Якщо begin > size_m -- кидає виключення std::out_of_range.
+    //! Але якщо стрічка містить менше символів для стирання,
+    //! ніж потрібно -- це не помилка, видаляє всі до кінця.
+    void erase(size_t begin, size_t size);
+
+    //! Наступні функції повертають розмір стрічки та
+    //! зарезервований блок пам'яті.
+    //! Правильніше б назвати get_size() та get_capacity(),
+    //! але історично в std::string прийнято
+    //! так, то будемо наслідувати його
+    size_t size() const noexcept;
+    size_t capacity() const noexcept;
+
+    //! Повертає вказівник на С-стрічку, яка відповідає
+    //! цьому об'єкту. Якщо виділяти один зайвий байт,
+    //! як описано в документації на конструтор, то їй
+    //! достатньо зберегти нульовий байт у позиції
+    //! data_m[size_m] і повернути data_m.
+    const char* c_str() const;
+
+    //! Функції find шукають вказану літеру чи стрічку,
+    //! починаючи із idx. Якщо не знайдено, повертає
+    //! константу my_str_t::not_found
+    //! Обробка помилок:
+    //! Якщо idx > size_m -- кидає виключення std::out_of_range.
+    static constexpr size_t not_found = -1;
+    size_t find(char c, size_t idx = 0);
+    size_t find(const std::string& str, size_t idx = 0);
+    size_t find(const char* cstr, size_t idx = 0);
+
+    //! Скопіювати підстрічку, із begin включно,
+    //! по begin+size не включно ([begin, begin+size)).
+    //! Якщо end за межами початкової стрічки -- це не помилка,
+    //! копіювати всі символи до кінця.
+    //! beg має бути в її межах -- якщо begin>size, це помилка.
+    //! Обробка помилок:
+    //! Якщо begin > size_m -- кидає виключення std::out_of_range.
+    my_str_t substr(size_t begin, size_t size);
+
+
+    // Author: Kassiia Tserkovna
+    friend std::ostream& operator<<(std::ostream& stream, const my_str_t& str);
+    // Author: Kassiia Tserkovna
+    friend std::istream& operator>>(std::istream& stream, my_str_t& str);
+
+    friend std::istream& readline(std::istream& stream, my_str_t& str);
+};
+
+bool operator==(const my_str_t& str1, const my_str_t& str2);
+bool operator!=(const my_str_t& str1, const my_str_t& str2);
+bool operator> (const my_str_t& str1, const my_str_t& str2);
+bool operator>=(const my_str_t& str1, const my_str_t& str2);
+bool operator< (const my_str_t& str1, const my_str_t& str2);
+bool operator<=(const my_str_t& str1, const my_str_t& str2);
+
+bool operator==(const my_str_t& str1, const char* cstr2);
+bool operator!=(const my_str_t& str1, const char* cstr2);
+bool operator> (const my_str_t& str1, const char* cstr2);
+bool operator>=(const my_str_t& str1, const char* cstr2);
+bool operator< (const my_str_t& str1, const char* cstr2);
+bool operator<=(const my_str_t& str1, const char* cstr2);
+
+bool operator==(const char* cstr1, const my_str_t& str2);
+bool operator!=(const char* cstr1, const my_str_t& str2);
+bool operator> (const char* cstr1, const my_str_t& str2);
+bool operator>=(const char* cstr1, const my_str_t& str2);
+bool operator< (const char* cstr1, const my_str_t& str2);
+bool operator<=(const char* cstr1, const my_str_t& str2);
+my_str_t operator+(const my_str_t& str1, const my_str_t& str2);
+my_str_t operator+(const char* cstr1, const my_str_t& str2);
+my_str_t operator+(const my_str_t& str1, const char* cstr2);
+my_str_t operator*(const my_str_t& str, size_t num);
+#endif
